9d335806560e9029f58f064109562608732e0369
Richard <mavahu@mediapro.vn>
2016-10-10 09:17:51 +0700

    Create addtext-3.js

---
+++ addtext-3.js
@@ -0,0 +1,9 @@
+/* <![CDATA[ */
+	$(function(){
+		$('h1').append(decodeURI(getQueryVariable('t')));
+		$('h2').append(decodeURI(getQueryVariable('t')));
+		$('h4').append(decodeURI(getQueryVariable('t')));
+		$('h5').append(decodeURI(getQueryVariable('t')));
+		$('h6').append(decodeURI(getQueryVariable('t')));
+	});
+/* ]]> */
