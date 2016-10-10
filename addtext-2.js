af8534332b4ac50aa0ce70f118278b064b3bbbab
Richard <mavahu@mediapro.vn>
2016-10-10 09:16:06 +0700

    addtext-2.js

---
+++ addtext-2.js
@@ -0,0 +1,17 @@
+/* <![CDATA[ */
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return ' - ' + pair[1];
+    }
+  }
+  return '';
+}
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+/* ]]> */
